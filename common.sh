6ff5d8896d00532cb28c02449e022899c737bd0d
Babu Natarajan <babu@amtexsystems.com>
2017-12-27 15:39:52 -0500

    S3 bucket policy update

--- common.sh
+++ common.sh
@@ -1,4 +1,8 @@
 #!bin/bash
+#Author :  Babu Natarajan
+#Created on : May 24th 2016
+#Updated S3 bucket policy on Dec 27th 2017
+
 
 #[Stamps the file name with a date]
 export TS=`date +%m-%d-%y-%H%M`
@@ -56,30 +60,25 @@ then
 fi
 
 #[Update more IP if GET access requireded for S3 content example "x.x.x.x/32"]
+export IPs='"1.1.1.1/32","2.2.2.2/32"'
 
 printf '{
-        "Version": "2008-10-17",
+        "Version": "2012-10-17",
         "Id": "S3PolicyId1",
         "Statement": [
                 {
-                        "Sid": "PutObject",
+                        "Sid": "AllowIP",
                         "Effect": "Allow",
                         "Principal": {
                                 "AWS": "*"
                         },
-                        "Action": "s3:PutObject",
-                        "Resource": "arn:aws:s3:::'$S3_BUCKET'/*"
-                },
-                {
-                        "Sid": "IPDeny",
-                        "Effect": "Deny",
-                        "Principal": {
-                                "AWS": "*"
-                        },
-                        "Action": "s3:GetObject",
+                        "Action": [
+				"s3:GetObject",
+				"s3:PutObject",
+				"s3:PutObjectAcl",
                         "Resource": "arn:aws:s3:::'$S3_BUCKET'/*",
                         "Condition": {
-                                "NotIpAddress": {
+                                "IpAddress": {
                                         "aws:SourceIp": ['$IPs']
                                 }
                         }
@@ -92,7 +91,8 @@ export CNT=`/usr/bin/aws s3api list-buckets | grep $S3_BUCKET | wc -l`
 if [[ $CNT == 0 ]]
 then
     echo "$S3_BUCKET doesn\'t exist creating..."
-    aws s3api create-bucket --bucket $S3_BUCKET  --region us-east-1 --acl public-read
+#    aws s3api create-bucket --bucket $S3_BUCKET  --region us-east-1 --acl public-read
+    aws s3api create-bucket --bucket $S3_BUCKET  --region us-east-1
 fi
     aws s3api put-bucket-policy --bucket $S3_BUCKET --policy file://$SPATH/policy.json
 
